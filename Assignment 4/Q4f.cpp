f7d055bf4ec874b8ca5f90459f1c676b70551511
Manya Garg <gmanya014@gmail.com>
2026-04-27 01:35:10 +0530

    Assingment 4

--- Assignment 4/Q4f.cpp
+++ Assignment 4/Q4f.cpp
@@ -1,50 +1,37 @@
-//Q4f - Dynamic memory allocation for array of class objects using new and delete
-
 #include <iostream>
 using namespace std;
-
 class Student {
 private:
     int rollNo;
     string name;
-
 public:
     Student() {
         rollNo = 0;
         name = "Unknown";
     }
-
     Student(int r, string n) {
         rollNo = r;
         name = n;
         cout << "Constructor called for " << name << endl;
     }
-
     ~Student() {
         if (rollNo != 0) {
             cout << "Destructor called for " << name << endl;
         }
     }
-
     void setData(int r, string n) {
         rollNo = r;
         name = n;
     }
-
     void display() {
         cout << "Roll No: " << rollNo << ", Name: " << name << endl;
-    }
-};
-
+    }};
 int main() {
-    cout << "Dynamic Memory Allocation for Array of Objects\n\n";
-
+    cout << "Dynamic Memory Allocation for Array of Objects\n";
     int n;
     cout << "Enter number of students: ";
     cin >> n;
-
     Student* students = new Student[n];
-
     for (int i = 0; i < n; i++) {
         int roll;
         string name;
@@ -55,15 +42,10 @@ int main() {
         getline(cin, name);
         students[i].setData(roll, name);
     }
-
     cout << "\nStudent Details:\n";
     for (int i = 0; i < n; i++) {
         students[i].display();
     }
-
     delete[] students;
-
-    cout << "\nMemory deallocated successfully using delete[].\n";
-
     return 0;
 }
