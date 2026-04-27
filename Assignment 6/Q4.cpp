6b44e2d3e0a05e7f81d63e32959100c8b9b69397
Manya Garg <gmanya014@gmail.com>
2026-04-27 10:17:39 +0530

    Assignment 6

--- Assignment 6/Q4.cpp
+++ Assignment 6/Q4.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
 using namespace std;
-
-class B; // forward declaration
-
+class B;
 class A {
-    int x;
+int x;
 public:
-    A(int x) { this->x = x; }
-
-    friend int add(A, B);
+A(int x) { this->x = x; }
+friend int add(A, B);
 };
-
 class B {
-    int y;
+int y;
 public:
-    B(int y) { this->y = y; }
-
-    friend int add(A, B);
-};
-
+B(int y) { this->y = y; }
+friend int add(A, B);};
 int add(A a, B b) {
-    return a.x + b.y;
-}
-
+return a.x + b.y;}
 int main() {
-    A a(10);
-    B b(20);
-
-    cout << "Sum = " << add(a, b);
-
-    return 0;
+A a(10);
+B b(20);
+cout << "Sum"<< add(a, b);
+return 0;
 }
